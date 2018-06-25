f6cf6233080bf6626713270ddc83282a50197eef
Ann Kim <annkim822@gmail.com>
2018-06-25 00:13:58 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_names = data.values.each_with_object([]) {|property_hash, names_array| names_array << property_hash.values}.flatten.uniq
   pigeon_keys = data.keys
-
-  #create a new hash where each key is a pigeon name, and each value is a hash
-  #and that hash's keys are the pigeon data keys = an empty array
+  
   new_hash = pigeon_names.each_with_object({}) do |name, hash|
     hash[name] = Hash.new
     pigeon_keys.each do |key|
@@ -13,11 +11,7 @@ def nyc_pigeon_organizer(data)
     end
   end
 
-
-  #for every pigeon nested within the passed hash
-  #add the property for which its listed under
-  #to the new hash
-  pigeon_data.each do |attr_key, property_hash|
+  data.each do |attr_key, property_hash|
     property_hash.each do |property_key, names_array|
       names_array.each do |pigeon|
         new_hash[pigeon][attr_key] << property_key.to_s 
